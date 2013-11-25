344044b0bf1a17c0284086e58e6346f231df963d
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-25 11:06:22 +0000

    Import of SHARYANTO/Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy-0.03 from CPAN.
    gitpan-cpan-distribution: Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Log/Any/Adapter/Core/Patch/UseDataDumperPerltidy.pm
+++ lib/Log/Any/Adapter/Core/Patch/UseDataDumperPerltidy.pm
@@ -10,7 +10,7 @@ use base qw(Module::Patch);
 
 use Data::Dumper::Perltidy;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 our %config;
 
@@ -38,9 +38,11 @@ sub patch_data {
 1;
 # ABSTRACT: Use Data::Dumper::Perltidy
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -48,7 +50,7 @@ Log::Any::Adapter::Core::Patch::UseDataDumperPerltidy - Use Data::Dumper::Perlti
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 SYNOPSIS
 
@@ -77,16 +79,31 @@ L<Log::Any::Adapter::Core::Patch::SetDumperIndent>
 
 L<Log::Any::Adapter::Core::Patch::UseDataDump>
 
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
 =head1 AUTHOR
 
 Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
--- lib/Log/Any/PT.pm
+++ lib/Log/Any/PT.pm
@@ -1,11 +1,14 @@
 package Log::Any::PT;
 use Log::Any::Adapter::Core::Patch::UseDataDumperPerltidy;
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 1;
 # ABSTRACT: Shortcut for Log::Any::Adapter::Core::Patch::UseDataDumperPerltidy
 
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -13,7 +16,23 @@ Log::Any::PT - Shortcut for Log::Any::Adapter::Core::Patch::UseDataDumperPerltid
 
 =head1 VERSION
 
-version 0.02
+version 0.03
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Log-Any-Adapter-Core-Patch-UseDataDumperPerltidy>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -21,10 +40,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
